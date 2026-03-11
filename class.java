42f9df4cfcab73ac762dbdcf0d6396a2135febc1
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-11 18:15:47 +0530

    Class and object

--- class.java
+++ class.java
@@ -1,3 +1,17 @@
+class Car {
+    String name;
+    int number;
+}
+
 public class Class {
-    
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+
+        myCar.name = "Toyota";
+        myCar.number = 1201;
+
+        System.out.println(myCar.name);
+        System.out.println(myCar.number);
+    }
 }
