4dfbe12669379f691690b753ae7432c1f58d5b9a
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-11 18:38:22 +0530

    update

--- constructor.java
+++ constructor.java
@@ -15,6 +15,6 @@ public class constructor {
 
         System.out.println(s1.name);
         System.out.println(s2.name);
-         System.out.println(s3.name);
+        System.out.println(s3.name);
     }
 }
