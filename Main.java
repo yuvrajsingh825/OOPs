36b54e0ee23e3b00e3d94f04627134bb7a44564a
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-11 19:12:33 +0530

    push example.java and Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Student {
+
+    String name;
+
+    Student(String name){
+        name = name;
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Rahul");
+
+        System.out.println(s1.name);
+    }
+
+}
